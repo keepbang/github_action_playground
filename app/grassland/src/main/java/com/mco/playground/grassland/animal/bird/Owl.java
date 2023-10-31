00fc49770a593d84e321a0fe4b706a92474f1956
sghwang <125856543+sghwang-mcorpor@users.noreply.github.com>
2023-10-31 15:12:11 +0900

    feat: Add owl class
    부엉이의 울음소리도 추가하였습니다.

---
+++ app/grassland/src/main/java/com/mco/playground/grassland/animal/bird/Owl.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2023-2023 MCorporation(https://mcorpor.com/) - All Rights Reserved.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ * Written by dev team, sghwang<sg.hwang@mcorpor.com>
+ * Last modified on 2023/10/31
+ */
+
+package com.mco.playground.grassland.animal.bird;
+
+import com.mco.playground.shared.animal.Bird;
+
+/**
+ * create on 10/31/23. create by IntelliJ IDEA.
+ *
+ * <p> 클래스 설명 </p>
+ *
+ * @author SeokGyu Hwang
+ * @version 1.0
+ * @since 1.0
+ */
+public class Owl extends Bird {
+
+  public Owl() {
+    super("부엉이", "부엉 부엉 올");
+  }
+}
