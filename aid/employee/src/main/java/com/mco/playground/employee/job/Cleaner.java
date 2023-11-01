07ccaee929e8fbe6f19a1bec109323ab0b86152b
keepbang <gb.chae@mcorpor.com>
2023-11-01 15:55:52 +0900

    ci: 청소부 객체 추가
    - 청소부 객체를 추가하였습니다.

--- aid/employee/src/main/java/com/mco/playground/employee/job/Cleaner.java
+++ aid/employee/src/main/java/com/mco/playground/employee/job/Cleaner.java
@@ -6,7 +6,7 @@ import com.mco.playground.employee.common.Person;
  * create on 2023/11/01.
  * create by IntelliJ IDEA.
  *
- * <p> 클래스 설명 </p>
+ * <p> 청소부 객체 </p>
  *
  * @author Gibyoung Chae (keepbang)
  * @version 1.0
